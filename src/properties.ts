a747252ff44e265e7e2ba1f8be9245eb0484e362
papamarkou <theodore.papamarkou@gmail.com>
2025-09-19 19:21:22 +0300

    Updated API_ROOT

--- src/properties.ts
+++ src/properties.ts
@@ -1,4 +1,4 @@
 // Simple properties file for app-wide constants
-export const API_ROOT = "https://polyshape.com/api";
+export const API_ROOT = "https://www.polyshape.com/api";
 // export const API_ROOT = "http://localhost:3000/api";
 
